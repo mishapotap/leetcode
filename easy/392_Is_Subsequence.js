11aaecc6141db9d7a205c25fb0ccc473e018acf3
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-10-11 12:20:26 +0300

    Solutions

---
+++ easy/392_Is_Subsequence.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/is-subsequence/
+// let s = "abc",
+//   t = "ahbgdc",
+//   s2 = "axc",
+//   t2 = "ahbgdc";
+// // Output: true / false
+// var isSubsequence = function (s, t) {
+//   if (!s) return true;
+//   let pointerS = 0;
+//   for (let i = 0; i < t.length; i++) {
+//     if (t[i] === s[pointerS]) pointerS++;
+//     if (pointerS === s.length) return true;
+//   }
+//   return false;
+// };
+// console.log(isSubsequence(s, t));

556e992700cd0eb5faed2665f8815362be9a5c19
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-12-18 10:15:54 +0300

    Solutions

---
+++ easy/1913_Maximum_Product_Difference_Between_Two_Pairs.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs
+// let nums = [5, 6, 2, 7, 4];
+// // Output: 34
+// var maxProductDifference = function (nums) {
+//   nums.sort((a, b) => a - b);
+//   return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+// };
+// console.log(maxProductDifference(nums));

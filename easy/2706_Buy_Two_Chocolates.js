a14ca92f0a593a4c931787d8a19f01004f645557
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2023-12-20 10:04:15 +0300

    Solutions

---
+++ easy/2706_Buy_Two_Chocolates.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/buy-two-chocolates/
+// let prices = [1, 2, 2],
+//   money = 3,
+//   prices2 = [3, 2, 3],
+//   money2 = 3;
+// // Output: 0 / 3
+// var buyChoco = function (prices, money) {
+//   prices.sort((a, b) => a - b);
+//   const leftover = money - prices[0] - prices[1];
+//   return leftover < 0 ? money : leftover;
+// };
+// console.log(buyChoco(prices2, money2));

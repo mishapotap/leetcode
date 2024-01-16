b2ebf400d61be8afda4efd2e6ef189dc7c9659b0
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-01-16 12:15:05 +0300

    Solutions

---
+++ easy/1047_Remove_All_Adjacent_Duplicates_In_String.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+// let s = "abbaca",
+//   s2 = "azxxzy";
+// // Output: "ca" / "ay"
+// var removeDuplicates = function (s) {
+//   let stack = [];
+//   for (const char of s) {
+//     char === stack.at(-1) ? stack.pop() : stack.push(char);
+//   }
+//   return stack.join("");
+// };
+// console.log(removeDuplicates(s));

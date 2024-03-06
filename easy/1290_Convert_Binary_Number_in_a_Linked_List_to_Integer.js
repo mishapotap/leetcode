b4d803cdf3080861f299bf68ad533a3227bd8ba9
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-03-06 13:06:14 +0300

    Solutions

---
+++ easy/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+// let head = [1, 0, 1];
+// // Output: 5
+// var getDecimalValue = function (head) {
+//   let str = "";
+//   while (head !== null) {
+//     str += head.val;
+//     head = head.next;
+//   }
+//   return parseInt(str, 2);
+// };
+// console.log(getDecimalValue(head));

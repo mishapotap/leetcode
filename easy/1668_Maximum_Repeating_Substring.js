93640810fcd18da6a203fa539e59d94dd8784db8
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-03-13 11:34:11 +0300

    Solutions

---
+++ easy/1668_Maximum_Repeating_Substring.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-repeating-substring/description/
+// let sequence = "ababc",
+//   word = "ab",
+//   sequence2 = "ababc",
+//   word2 = "ba";
+// // Output: 2 / 1
+// var maxRepeating = function (sequence, word) {
+//   let res = 0;
+//   let str = word;
+//   while (sequence.includes(str)) {
+//     res++;
+//     str += word;
+//   }
+//   return res;
+// };
+// console.log(maxRepeating(sequence, word));

3664ab38ee7ffcbc65f0b1ce7727f7e433fff2df
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-04-02 16:47:57 +0300

    Solutions

--- easy/205_Isomorphic_Strings.js
+++ easy/205_Isomorphic_Strings.js
@@ -10,17 +10,27 @@
 // // Output: true / false / true / false / false
 // // s can be replaced to get t
 // var isIsomorphic = function (s, t) {
+//   // let map = new Map();
+//   // let str = "";
+//   // for (let i = 0; i < s.length; i++) {
+//   //   if (map.has(s[i])) {
+//   //     if (map.get(s[i]) !== t[i]) return false;
+//   //   } else {
+//   //     if (str.includes(t[i])) return false;
+//   //     map.set(s[i], t[i]);
+//   //     str += t[i];
+//   //   }
+//   // }
+//   // return true;
+//   // Второе решение
 //   let map = new Map();
-//   let str = "";
 //   for (let i = 0; i < s.length; i++) {
-//     if (map.has(s[i])) {
-//       if (map.get(s[i]) !== t[i]) return false;
-//     } else {
-//       if (str.includes(t[i])) return false;
-//       map.set(s[i], t[i]);
-//       str += t[i];
-//     }
+//     const S = "s" + s[i];
+//     const T = "t" + t[i];
+//     if (!map.has(S)) map.set(S, t[i]);
+//     if (!map.has(T)) map.set(T, s[i]);
+//     if (t[i] !== map.get(S) || s[i] !== map.get(T)) return false;
 //   }
 //   return true;
 // };
-// console.log(isIsomorphic(s3, t3));
+// console.log(isIsomorphic(s4, t4));

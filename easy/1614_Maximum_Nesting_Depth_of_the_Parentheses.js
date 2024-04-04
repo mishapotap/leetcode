68728cf34af1849c115d20c00dd68ed80355ea87
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-04-04 11:24:56 +0300

    Solutions

--- easy/1614_Maximum_Nesting_Depth_of_the_Parentheses.js
+++ easy/1614_Maximum_Nesting_Depth_of_the_Parentheses.js
@@ -4,12 +4,22 @@
 //   s3 = "+(+(+)+)+";
 // // Output: 3 / 3 / 2
 // var maxDepth = function (s) {
-//   let current = 0;
-//   let res = 0;
+//   // Решение по скорости 0(n) по памяти 0(1)
+//   // let current = 0;
+//   // let res = 0;
+//   // for (const char of s) {
+//   //   if (char === "(") res = Math.max(res, ++current);
+//   //   if (char === ")") current--;
+//   // }
+//   // return res;
+//   // Решение по скорости 0(n) по памяти 0(n) через стек
+//   let stack = [];
+//   let max = 0;
 //   for (const char of s) {
-//     if (char === "(") res = Math.max(res, ++current);
-//     if (char === ")") current--;
+//     if (char === "(") stack.push(char);
+//     if (char === ")") stack.pop();
+//     if (stack.length > max) max = stack.length;
 //   }
-//   return res;
+//   return max;
 // };
 // console.log(maxDepth(s3));

c13257a8f9bb5b6e8df475a7d80554fc9d3f4d9a
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-06-27 12:25:40 +0300

    Solutions

---
+++ easy/1704_Determine_if_String_Halves_Are_Alike.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/determine-if-string-halves-are-alike/
+// let s = "book";
+// // Output: true
+// let set = new Set("aeiouAEIOU");
+// var halvesAreAlike = function (s) {
+//   let left = 0,
+//     right = 0;
+//   for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+//     if (set.has(s[i])) left++;
+//     if (set.has(s[j])) right++;
+//   }
+//   return left === right;
+// };
+// console.log(halvesAreAlike(s));
---
+++ easy/3151_Special_Array_I.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/special-array-i/
+// let nums = [1],
+//   nums2 = [2, 1, 4], nums3 = [4,3,1,6]
+// Output: true / true / false;
+// var isArraySpecial = function (nums) {
+//   let isOdd = nums[0] % 2;
+//   for (let i = 1; i < nums.length; i++) {
+//     const current = nums[i] % 2;
+//     if (current === isOdd) return false;
+//     isOdd = current;
+//   }
+//   return true;
+// };
+// console.log(isArraySpecial(nums))

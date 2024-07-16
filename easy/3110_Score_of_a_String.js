e1d9d401e5e963ebb50b0afac0c14ba0f35d60d5
Mikhail Potapov <mikhail.s.potapov@gmail.com>
2024-07-16 11:07:14 +0300

    Solutions

---
+++ easy/3110_Score_of_a_String.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/score-of-a-string/
+// let s = "hello";
+// // Output: 13
+// var scoreOfString = function (s) {
+//   let score = 0;
+//   for (let i = 1; i < s.length; i++) {
+//     score += Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i));
+//   }
+//   return score;
+// };
+// console.log(scoreOfString(s));
